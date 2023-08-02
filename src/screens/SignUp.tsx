e8fc8a00bc45f6d797bbdd988e49c88fb61b836a
“Lelo <lelo@tonavilla.com.br>
2023-08-02 20:42:15 -0300

    Telas de SignIn e SignUp criadas e rodando perfeitamente.

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,11 +1,20 @@
+import { useNavigation } from '@react-navigation/native';
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
+
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
 export function SignUp(){
+
+    const navigation = useNavigation();
+
+    function handleGoBack(){
+        navigation.goBack();
+    }
+
     return(
         <ScrollView
             contentContainerStyle={{flexGrow: 1}}
@@ -14,6 +23,7 @@ export function SignUp(){
             <VStack flex={1} px={10} pb={16}>
                 <Image
                     source={BackgroundImg}
+                    defaultSource={BackgroundImg}
                     alt='Pessoas treinando'
                     resizeMode='contain'
                     position='absolute'
@@ -51,13 +61,14 @@ export function SignUp(){
                         placeholder='Senha'
                         secureTextEntry 
                     />
-                    <Button title='Acessar'/>
+                    <Button title='Criar e acessar'/>
                 </Center>
 
                 <Button
-                    title='Criar e acessar'
+                    title='Voltar para o login'
                     variant='outline'
                     mt={24}
+                    onPress={handleGoBack}
                 />
             </VStack>
         </ScrollView>
